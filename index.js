c4369cb8a9929ba6b973da36d0fc3b7e3be9c2a7
Andrew Kelley <superjoe30@gmail.com>
2014-02-03 14:05:39 -0500

    convert to js and simplify

---
+++ index.js
@@ -0,0 +1,43 @@
+var crypto = require('crypto');
+
+module.exports = Shavaluator;
+
+function Shavaluator(redisClient) {
+  this.redisClient = redisClient;
+  this.scripts = {};
+}
+
+Shavaluator.prototype.add = function(name, body) {
+  this.scripts[name] = {
+    body: body,
+    hash: sha1(body),
+  };
+};
+
+Shavaluator.prototype.exec = function(name, keys, args, callback) {
+  var redisClient = this.redisClient;
+  var script = this.scripts[name];
+  if (!script) return process.nextTick(errorScriptNotFound);
+
+  var params = [script.hash, keys.length].concat(keys).concat(args);
+  redisClient.evalsha(params, function(err, res) {
+    if (err) {
+      if (/^NOSCRIPT /.test(err.message)) {
+        var params = [script.body, keys.length].concat(keys).concat(args);
+        redisClient.eval(params, callback);
+      } else {
+        callback(err, res);
+      }
+    } else {
+      callback(null, res);
+    }
+  });
+
+  function errorScriptNotFound() {
+    callback(new Error("unrecognized script name: " + name));
+  }
+};
+
+function sha1(string) {
+  return crypto.createHash('sha1').update(string, 'utf8').digest('hex');
+}
